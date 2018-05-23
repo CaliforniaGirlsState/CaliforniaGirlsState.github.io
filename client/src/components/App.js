459a6c4837f811ba7d93d4e69e05d057d2f0cd15
Kabin Nam <kabinnam@berkeley.edu>
2018-05-23 01:16:53 -0700

    Add CSSBaseline for material-ui. Normalizes browser styling, and fixes the margin issue with AppBar.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,22 +4,26 @@ import MenuAppBar from './MenuAppBar';
 import Access from './Access';
 import Notices from './Notices';
 import Vote from './Vote';
+import CssBaseline from 'material-ui/CssBaseline';
 
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-    			<MenuAppBar />
-          <div style={{maxWidth: '90%', margin: 'auto', padding: '20px 0'}}>
-            <Route exact path="/" component={Access} />
-            {/* Need to change /Notices to /, because it should share with access.. */}
-            <Route path="/Notices" component={Notices} />
-            <Route path="/Vote" component={Vote} />
+      <React.Fragment>
+        <CssBaseline />
+        <BrowserRouter>
+          <div>
+      			<MenuAppBar />
+            <div style={{maxWidth: '90%', margin: 'auto', padding: '20px 0'}}>
+              <Route exact path="/" component={Access} />
+              {/* Need to change /Notices to /, because it should share with access.. */}
+              <Route path="/Notices" component={Notices} />
+              <Route path="/Vote" component={Vote} />
+            </div>
           </div>
-        </div>
-	  	</BrowserRouter>
+  	  	</BrowserRouter>
+      </React.Fragment>
     );
   }
 }
